bf96e6c177e0ace9378f594124cc3faf185e716a
0rishisingh0 <41266636+0rishisingh0@users.noreply.github.com>
2018-07-15 23:42:53 -0700

    calculator.py
    calculator

---
+++ calculator.py
@@ -0,0 +1,45 @@
+#this program is written by ☺rishi singh☺
+def add(x, y):
+   return x + y
+def subtract(x, y):
+   return x - y
+def multiply(x, y):
+   return x * y
+def divide(x, y):
+   return x / y
+def average(x, y):
+   return (x+y)/2
+def power(x, y):
+   return x**y
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Average")
+print("6.Power xY")
+choice = input("Enter choice(1/2/3/4/5/6):")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+
+elif choice == '5':
+   print(num1,"+",num2,"/ 2 =", average(num1,num2))
+elif choice == '6':
+   print(num1,"to the power",num2,"=", power(num1,num2))
+   
+else:
+   print("Invalid input")
+input("ok")
+
